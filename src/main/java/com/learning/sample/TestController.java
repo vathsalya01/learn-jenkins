7ff97445ed4bfe9a2af5c0a26e2245efc3cbfedb
vathsalya01 <81856395+vathsalya01@users.noreply.github.com>
2022-05-05 17:50:28 +0530

    Add files via upload

---
+++ src/main/java/com/learning/sample/TestController.java
@@ -0,0 +1,16 @@
+package com.learning.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String firstHandler() {
+		return "fistHandler() method";
+	}
+
+}
---
+++ src/test/java/com/learning/sample/SpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
